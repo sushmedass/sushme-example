4d6d53018ad5e13d5e4f27b5595c58702ebab85d
sushmedass <78635716+sushmedass@users.noreply.github.com>
2023-05-26 12:11:39 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "test.h"
+
+void helloFunc()
+{
+  printf("Hi this is Sushme's code\n");
+}
