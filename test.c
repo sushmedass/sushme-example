6dadd3f0875865a63925c42c81c5084eed85c046
sushmedass <78635716+sushmedass@users.noreply.github.com>
2023-06-05 12:31:46 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,20 +1,9 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
-#include <linux/i2c-dev.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 #include "test.h"
 
-void helloFunc()
+int main(int argc, char *argv[])
 {
-  printf("Hi this is Sushme's code\n");
+  printf("Hello World!!!!!\n");
+  return 0;
 }
